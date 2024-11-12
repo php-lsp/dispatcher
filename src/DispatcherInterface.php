75e10cc45784b0119d544995ba56b17a53465cc8
SerafimArts <nesk@xakep.ru>
2024-11-12 18:03:24 +0000

    Revert local packages

---
+++ src/DispatcherInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lsp\Dispatcher;
+
+use Lsp\Contracts\Rpc\Message\NotificationInterface;
+use Lsp\Contracts\Rpc\Message\RequestInterface;
+use Lsp\Contracts\Rpc\Message\ResponseInterface;
+
+interface DispatcherInterface
+{
+    /**
+     * Notify server by a notification message.
+     *
+     * This method MUST not throw any exception. In case of any notification
+     * errors, an exception should be returned as a result.
+     */
+    public function notify(NotificationInterface $notification): ?\Throwable;
+
+    /**
+     * Execute the method by a request message.
+     *
+     * @template TArgIdentifier of mixed
+     *
+     * @param RequestInterface<TArgIdentifier> $request
+     *
+     * @return ResponseInterface<TArgIdentifier>
+     */
+    public function call(RequestInterface $request): ResponseInterface;
+}
